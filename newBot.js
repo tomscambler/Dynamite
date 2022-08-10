2c7ae098bc2286d46bd80d3ad4fcea91f022db7f
Rosalie van Onzenoort <r.vanonzenoort98@gmail.com>
2022-08-10 10:58:09 +0100

    adding previous code

--- newBot.js
+++ newBot.js
@@ -1,8 +1,20 @@
 class Bot {
     makeMove(gamestate) {
-        // hello Tom
+        // weight previous moves more heavily but take into account all previous moves
+        // after that, think about how and when to deploy the dynamite (after a certain number of draws, and taking into account the max number of dynamites useable)
 
-        whatisthis = gamestate.rounds;
+        if (gamestate.rounds.length ===0) {
+            this.dynamite = 100;
+            this.roundCount = 0;
+            this.p1previousoutput = []
+            output =  ['P','R','S'][Math.floor(Math.random() * 3)];
+        }
+
+        
+
+        this.p1previousoutput.push(output)
+        
+        return output;
 
 
 
